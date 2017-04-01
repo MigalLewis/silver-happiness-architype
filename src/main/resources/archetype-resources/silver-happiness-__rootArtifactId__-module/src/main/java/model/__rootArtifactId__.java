834ef0b404e8b580a2da8b864b9be168ab721c6d
Migal Lewis <migal.lewis@standardbank.co.za>
2017-04-01 13:37:46 +0200

    Added model and exception

---
+++ src/main/resources/archetype-resources/silver-happiness-__rootArtifactId__-module/src/main/java/model/__rootArtifactId__.java
@@ -0,0 +1,14 @@
+#set( $symbol_pound = '#' )
+#set( $symbol_dollar = '$' )
+#set( $symbol_escape = '\' )
+package ${package}.model;
+
+import lombok.Data;
+/**
+ *
+ * @author migal
+ */
+@Data
+public class ${rootArtifactId} {
+  
+}
