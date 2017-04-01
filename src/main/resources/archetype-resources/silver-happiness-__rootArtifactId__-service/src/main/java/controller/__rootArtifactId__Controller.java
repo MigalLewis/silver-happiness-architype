455979861575cbf53835214174dc5d927e36c2e8
Migal Lewis <migal.lewis@standardbank.co.za>
2017-04-01 15:02:27 +0200

    exposed basic services

--- src/main/resources/archetype-resources/silver-happiness-__rootArtifactId__-service/src/main/java/controller/__rootArtifactId__Controller.java
+++ src/main/resources/archetype-resources/silver-happiness-__rootArtifactId__-service/src/main/java/controller/__rootArtifactId__Controller.java
@@ -4,12 +4,16 @@
 package ${package}.controller;
 
 
-import ${package}.${rootArtifactId}Module;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 import io.swagger.annotations.ApiOperation;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.ResponseBody;
+import ${package}.${rootArtifactId}Module;
+import ${package}.model.${rootArtifactId};
 
 /*
  * To change this license header, choose License Headers in Project Properties.
@@ -20,7 +24,8 @@ import io.swagger.annotations.ApiOperation;
  *
  * @author migal
  */
-@RestController(value="/rest/${rootArtifactId}")
+@RestController
+@RequestMapping(path = "/rest/${rootArtifactId}", produces = "application/json" )
 public class ${rootArtifactId}Controller {
 
     @Autowired
@@ -31,5 +36,47 @@ public class ${rootArtifactId}Controller {
     public String hello() {
         return module.sayHello();
     }
+    /**
+    *
+    * @param id
+    * @return
+    */
+    @RequestMapping(method = RequestMethod.GET, value = "/{id}")
+    @ApiOperation(value = "Finds ${rootArtifactId} by id")
+    @ResponseBody
+    public ${rootArtifactId} find${rootArtifactId}(@PathVariable long id) {
+      return module.find${rootArtifactId}(id);
+    }
+   /**
+    *
+    * @param request
+    */
+    @RequestMapping(method = RequestMethod.POST)
+    @ApiOperation(value = "Adds a ${rootArtifactId}")
+    @ResponseBody
+    public void add${rootArtifactId}(@RequestBody ${rootArtifactId} request){
+      module.add${rootArtifactId}(request);
+    }
+   /**
+    *
+    * @param id
+    */
+    @RequestMapping(method = RequestMethod.DELETE , value = "/{id}")
+    @ApiOperation(value = "Removes an ${rootArtifactId}")
+    public void delete${rootArtifactId}(@PathVariable long id){
+      module.remove${rootArtifactId}(id);
+    }
+    /**
+    *
+    * @param request
+    * @return
+    */
+    @RequestMapping(method = RequestMethod.PUT)
+    @ApiOperation(value = "edits a ${rootArtifactId}")
+    @ResponseBody
+    public ${rootArtifactId} update${rootArtifactId}(@RequestBody ${rootArtifactId} request){
+      return module.edit${rootArtifactId}(request);
+    }
+
 
 }
